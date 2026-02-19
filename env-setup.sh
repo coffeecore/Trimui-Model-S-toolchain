1712de8f8d68fc5d70b5bc8288c353d70266bcf3
Antoine Guillemin <antoine.guillemin@vesperiagroup.com>
2026-02-19 16:03:22 +0100

    Fix env setup script

--- env-setup.sh
+++ env-setup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-ENV SDK_ROOT="=/opt/trimui-toolchain"
-ENV TOOLCHAIN_PREFIX="arm-buildroot-linux-gnueabi"
-ENV TOOLBIN="${SDK_ROOT}/bin/${TOOLCHAIN_PREFIX}-"
-ENV SYSROOT="${SDK_ROOT}/${TOOLCHAIN_PREFIX}/sysroot"
-ENV CROSS_COMPILE="${TOOLBIN}"
-ENV CC="${TOOLBIN}gcc"
-ENV CXX="${TOOLBIN}g++"
-ENV LD="${TOOLBIN}ld"
-ENV AS="${TOOLBIN}as"
-ENV AR="${TOOLBIN}ar"
-ENV RANLIB="${TOOLBIN}ranlib"
-ENV STRIP="${TOOLBIN}strip"
+export SDK_ROOT="=/opt/trimui-toolchain"
+export TOOLCHAIN_PREFIX="arm-buildroot-linux-gnueabi"
+export TOOLBIN="${SDK_ROOT}/bin/${TOOLCHAIN_PREFIX}-"
+export SYSROOT="${SDK_ROOT}/${TOOLCHAIN_PREFIX}/sysroot"
+export CROSS_COMPILE="${TOOLBIN}"
+export CC="${TOOLBIN}gcc"
+export CXX="${TOOLBIN}g++"
+export LD="${TOOLBIN}ld"
+export AS="${TOOLBIN}as"
+export AR="${TOOLBIN}ar"
+export RANLIB="${TOOLBIN}ranlib"
+export STRIP="${TOOLBIN}strip"
 # Set pkg-config to look in the SDK's sysroot
-ENV PKG_CONFIG_PATH="${SYSROOT}/usr/lib/pkgconfig"
-ENV PKG_CONFIG_LIBDIR="${SYSROOT}/usr/lib/pkgconfig"
-ENV PKG_CONFIG_SYSROOT_DIR="${SYSROOT}"
+export PKG_CONFIG_PATH="${SYSROOT}/usr/lib/pkgconfig"
+export PKG_CONFIG_LIBDIR="${SYSROOT}/usr/lib/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="${SYSROOT}"
 # Add the toolchain's bin directory to the main PATH
-ENV PATH="${SDK_ROOT}/bin:${PATH}"
+export PATH="${SDK_ROOT}/bin:${PATH}"
 
 export CHAINPREFIX=/opt/trimui-toolchain
 export PATH=$PATH:$CHAINPREFIX/bin
